11aa8541f179bdbfd7ffa0787169e8cdf164d9e6
Parkcharito <100402443+Parkcharito@users.noreply.github.com>
2022-03-10 09:36:00 +0900

    음 이벤트 달력
    이벤트

--- wmsFinal/src/main/java/com/kh/wms/event/controller/EventController.java
+++ wmsFinal/src/main/java/com/kh/wms/event/controller/EventController.java
@@ -2,6 +2,8 @@ package com.kh.wms.event.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.kh.wms.event.model.service.EventService;
 
@@ -10,4 +12,13 @@ public class EventController {
 
 	@Autowired
 	private EventService eventService;
+	
+	
+	@RequestMapping(value="eventMain.ev")
+	public ModelAndView eventMain(ModelAndView mv) {
+		
+		mv.setViewName("event/eventMain");
+		
+		return mv;
+	}
 }
