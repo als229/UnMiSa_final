eff84924f5a7b11466250b72a3618123b20c76eb
placeman <83527522+als229@users.noreply.github.com>
2022-03-23 19:46:25 +0900

    어드레스 관민
    어드레스 커밋

--- wmsFinal/src/main/java/com/kh/wms/team/controller/TeamController.java
+++ wmsFinal/src/main/java/com/kh/wms/team/controller/TeamController.java
@@ -35,32 +35,7 @@ public class TeamController {
 	@RequestMapping("createTeamForm.te")
 	public ModelAndView createTeamForm(ModelAndView mv) {
 		
-		ArrayList<Address> aList = teamService.selectAddressList();
-		ArrayList<String> siDoList = new ArrayList<String>();
-		ArrayList<String> guList = new ArrayList<String>();
-		ArrayList<String> siGunList = new ArrayList<String>();
-		for(int i = 0; i < aList.size(); i++) {
-			
-			if(i != aList.size()-1) {
-				
-				if(!aList.get(i).getSiDo().equals(aList.get(i+1).getSiDo())) {
-					
-					siDoList.add(aList.get(i).getSiDo());
-				}
-			}
-			if(aList.get(i).getGu() != null) {
-				guList.add(aList.get(i).getGu());
-			}
-			if(aList.get(i).getSiGun() != null) {
-				siGunList.add(aList.get(i).getSiGun());
-			}
-		}
-		siDoList.add("제주특별시");
-		
-		mv.addObject("siGunList",siGunList);
-		mv.addObject("guList",guList);
-		mv.addObject("siDoList",siDoList);
-		mv.addObject("aList",aList);
+
 		mv.setViewName("team/createTeam");
 		
 		return mv;
